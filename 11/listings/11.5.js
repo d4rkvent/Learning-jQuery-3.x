abbbf836cdafb3c2c77f0bf7913211fffa80cbe8
Adam Boduch <adam.boduch@gmail.com>
2017-03-30 17:41:02 -0400

    Initial code

---
+++ 11/listings/11.5.js
@@ -0,0 +1,27 @@
+$.fx.speeds._default = 250;
+
+$(() => {
+  $('#fx-toggle')
+    .show()
+    .on('click', () => {
+      $.fx.off = !$.fx.off;
+    });
+
+  $('div.member')
+    .on('mouseenter mouseleave', ({ type, target }) => {
+      const width = height = type == 'mouseenter' ?
+        85 : 75;
+      const paddingTop = paddingLeft = type == 'mouseenter' ?
+        0 : 5;
+
+      $(target)
+        .find('img')
+        .stop()
+        .animate({
+          width,
+          height,
+          paddingTop,
+          paddingLeft
+        });
+    });
+});
